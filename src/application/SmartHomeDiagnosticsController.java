44f98009953f691649d40d13409e3fe6dd4b36b6
camrhys <camrhys@uab.edu>
2021-04-11 22:40:12 -0500

    Calculated simulations for the Diagnostics screen

--- src/application/SmartHomeDiagnosticsController.java
+++ src/application/SmartHomeDiagnosticsController.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -10,7 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class SmartHomeDiagnosticsController implements Initializable{
@@ -20,6 +25,12 @@ public class SmartHomeDiagnosticsController implements Initializable{
 	private Button HomeButton;
 	@FXML
 	private Button UsageButton;
+	@FXML
+    private Button simulatewashingButton;
+    @FXML
+    private Button simulateshowerButton;
+	@FXML
+    private TextArea simulationField;
 	
 	public void setHomeScene(Scene scene) {
 		firstScene = scene;
@@ -42,11 +53,103 @@ public class SmartHomeDiagnosticsController implements Initializable{
 		Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 		primaryStage.setScene(secondScene);
 	}
-
-	@Override
+	
+	// Calculates the shower event and outputs the results
+	@FXML
+    void simulateshowerButtonPressed(ActionEvent event) {
+		simulationField.clear();
+		simulationField.appendText("\n Calculating shower event...");
+		
+		// Shower simulation
+		List<Double> totals = simulationCalculation(0, 25, .33, 0.65);
+		totals = roundingData(totals);
+		
+		// TextArea output
+		String watts = "\n Calculated kilowatts used: ";
+		String gallons = "\n Calculated gallons used: ";
+		String cost = "\n Calculated overall cost: ";
+		String addon = Double.toString(totals.get(0));
+		simulationField.appendText(watts+addon);
+		addon = Double.toString(totals.get(1));
+		simulationField.appendText(gallons+addon);
+		addon = Double.toString(totals.get(2));
+		simulationField.appendText(cost+addon);
+    }
+	
+	// Calculates the dishwasher event and outputs the results
+    @FXML
+    void simulatewashingButtonPressed(ActionEvent event) {
+    	simulationField.clear();
+		simulationField.appendText("\n Calculating washing event...");
+		
+		// Washing with sink simulation
+		List<Double> totals = simulationCalculation(0, 25, .083, 1);
+		double sinkw = totals.get(0); 
+		double sinkg = totals.get(1);
+		double sinkc = totals.get(2);
+		
+		// Washing with dishwasher simulation
+		totals = simulationCalculation(1800, 6, .75, 1);
+		totals.set(0, totals.get(0)+sinkw);
+		totals.set(1, totals.get(1)+sinkg);
+		totals.set(2, totals.get(2)+sinkc);
+		totals = roundingData(totals);
+		
+		// TextArea output
+		String watts = "\n Calculated kilowatts used: ";
+		String gallons = "\n Calculated gallons used: ";
+		String cost = "\n Calculated overall cost: ";
+		String addon = Double.toString(totals.get(0));
+		simulationField.appendText(watts+addon);
+		addon = Double.toString(totals.get(1));
+		simulationField.appendText(gallons+addon);
+		addon = Double.toString(totals.get(2));
+		simulationField.appendText(cost+addon);
+    }
+	
+    // Calculates for any given simulation
+    public List<Double> simulationCalculation(int watts, int gallons, double time, double hotpercent) {
+		List<Double> totals = Arrays.asList(0.0, 0.0, 0.0);
+		double w = 0.0;
+		double g = gallons;
+		double c = 0.0;
+		
+		// watts calculation
+		if (watts != 0) {
+			w = w + ((watts*time)/1000);
+		}
+		
+		// hot water heater calculation
+		if (hotpercent != 0) {
+			w = w + (((4000*hotpercent) * (4*gallons))/1000);
+		}
+		
+		// cost calculation combining the wattage and water cost
+		c = c + (0.12 * w);
+		c = c + (2.52 * (g/748));
+		totals.set(0, w);
+		totals.set(1, g);
+		totals.set(2, c);
+		
+		return totals;
+    }
+	
+    public List<Double> roundingData(List<Double> totals) {
+		int z = 0;
+		double setas;
+		while (z <= 2) {
+			setas = totals.get(z);
+			setas = Math.round(setas*100.0)/100.0;
+			totals.set(z, setas);
+			z++;
+		}
+		return totals;
+	}
+    
+    @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-
+		
+    	
 	}
 
 	
