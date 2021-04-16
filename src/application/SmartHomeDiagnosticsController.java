4d430e19e4d7880eab43f0bf78b86bf0d4ac012d
Jessie Elkins <jelkins3@uab.edu>
2021-04-15 19:47:56 -0500

    Updated simulations to show on home page for set amount of time.

--- src/application/SmartHomeDiagnosticsController.java
+++ src/application/SmartHomeDiagnosticsController.java
@@ -75,110 +75,178 @@ public class SmartHomeDiagnosticsController implements Initializable{
 		}
 	}
 	
+	// pause function for waiting during simulations
+	public static void pause(int milisec) {
+		try {
+			Thread.sleep(milisec);
+		} catch (InterruptedException e) {
+			System.out.println("Error sleeping");
+		}
+	}
+	
 	// Calculates the shower event and outputs the results
 	@FXML
     public void simulateshowerButtonPressed(ActionEvent event) {
-		simulationField.clear();
+		new Thread() {
+			public void run() {
+				simulationField.clear();
 		
-		simulationMinutesUpdate();
-		ToggleButton buttonID = (ToggleButton) event.getSource();
+				// disabling buttons while simulation is running
+				simulateshowerButton.setDisable(true);
+				simulatewashingButton.setDisable(true);
+				
+				simulationMinutesUpdate();
+				ToggleButton buttonID = (ToggleButton) event.getSource();
 		
-		String simulateStart = "\n Calculating shower event for "+String.valueOf(timeToSimulate)+" minutes...";
-		simulationField.appendText(simulateStart);
+				String simulateStart = "\n Calculating shower event for "+String.valueOf(timeToSimulate)+" minutes...";
+				simulationField.appendText(simulateStart);
+				
+				// I just commented this out for now
+				/*if (buttonID.isSelected() == true) {
+					homeController.diagnosticToggle("Master Bedroom Shower", 1);
+					homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 1);
+					homeController.diagnosticToggle("Master Bath Overhead Light", 1);
+					homeController.diagnosticToggle("Appliance - Water Heater", 1);
+				} else if (buttonID.isSelected() == false) {
+					homeController.diagnosticToggle("Master Bedroom Shower", 2);
+					homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 2);
+					homeController.diagnosticToggle("Master Bath Overhead Light", 2);
+					homeController.diagnosticToggle("Appliance - Water Heater", 2);
+				} */
+				
+				// turning on shower, fan, and light
+				homeController.diagnosticToggle("Master Bedroom Shower", 1);
+				homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 1);
+				homeController.diagnosticToggle("Master Bath Overhead Light", 1);
+				
+				// waiting while person "takes" a shower
+				pause(10000);
+				
+				// turning off shower, fan, and light
+				homeController.diagnosticToggle("Master Bedroom Shower", 2);
+				homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 2);
+				homeController.diagnosticToggle("Master Bath Overhead Light", 2);
+				
+				// turning on water heater
+				homeController.diagnosticToggle("Appliance - Water Heater", 1);
+				
+				// waiting while water heater heats more water
+				pause(10000);
+				
+				// turning off water heater
+				homeController.diagnosticToggle("Appliance - Water Heater", 2);
+				
 		
-		if (buttonID.isSelected() == true) {
-			homeController.diagnosticToggle("Master Bedroom Shower", 1);
-			homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 1);
-			homeController.diagnosticToggle("Master Bath Overhead Light", 1);
-			homeController.diagnosticToggle("Appliance - Water Heater", 1);
-		} else if (buttonID.isSelected() == false) {
-			homeController.diagnosticToggle("Master Bedroom Shower", 2);
-			homeController.diagnosticToggle("Master Bedroom Exhaust Fan", 2);
-			homeController.diagnosticToggle("Master Bath Overhead Light", 2);
-			homeController.diagnosticToggle("Appliance - Water Heater", 2);
+				// Shower (+ Water Heater) calculations
+				List<Double> totals = simulationCalculation(0, 25, timeToSimulate/60, 0.65);
+				double tempw = totals.get(0); 
+				double tempg = totals.get(1);
+				double tempc = totals.get(2);
+		
+				// Exhaust fan calculations
+				totals = simulationCalculation(30, 0, timeToSimulate/60, 0.0);
+				totals.set(0, totals.get(0)+tempw);
+				totals.set(1, totals.get(1)+tempg);
+				totals.set(2, totals.get(2)+tempc);
+				tempw = totals.get(0); 
+				tempg = totals.get(1);
+				tempc = totals.get(2);
+		
+				// Overhead light calculations
+				totals = simulationCalculation(60, 0, timeToSimulate/60, 0.0);
+				totals.set(0, totals.get(0)+tempw);
+				totals.set(1, totals.get(1)+tempg);
+				totals.set(2, totals.get(2)+tempc);
+				totals = roundingData(totals);
+		
+				// TextArea output
+				String watts = "\n Calculated kilowatts used: ";
+				String gallons = "\n Calculated gallons used: ";
+				String cost = "\n Calculated overall cost: ";
+				String addon = Double.toString(totals.get(0));
+				simulationField.appendText(watts+addon);
+				addon = Double.toString(totals.get(1));
+				simulationField.appendText(gallons+addon);
+				addon = Double.toString(totals.get(2));
+				simulationField.appendText(cost+addon);
 		}
-		
-		// Shower (+ Water Heater) calculations
-		List<Double> totals = simulationCalculation(0, 25, timeToSimulate/60, 0.65);
-		double tempw = totals.get(0); 
-		double tempg = totals.get(1);
-		double tempc = totals.get(2);
-		
-		// Exhaust fan calculations
-		totals = simulationCalculation(30, 0, timeToSimulate/60, 0.0);
-		totals.set(0, totals.get(0)+tempw);
-		totals.set(1, totals.get(1)+tempg);
-		totals.set(2, totals.get(2)+tempc);
-		tempw = totals.get(0); 
-		tempg = totals.get(1);
-		tempc = totals.get(2);
-		
-		// Overhead light calculations
-		totals = simulationCalculation(60, 0, timeToSimulate/60, 0.0);
-		totals.set(0, totals.get(0)+tempw);
-		totals.set(1, totals.get(1)+tempg);
-		totals.set(2, totals.get(2)+tempc);
-		totals = roundingData(totals);
-		
-		// TextArea output
-		String watts = "\n Calculated kilowatts used: ";
-		String gallons = "\n Calculated gallons used: ";
-		String cost = "\n Calculated overall cost: ";
-		String addon = Double.toString(totals.get(0));
-		simulationField.appendText(watts+addon);
-		addon = Double.toString(totals.get(1));
-		simulationField.appendText(gallons+addon);
-		addon = Double.toString(totals.get(2));
-		simulationField.appendText(cost+addon);
-    }
+		}.start();
+	}
 	
 	// Calculates the dishwasher event and outputs the results
     @FXML
     void simulatewashingButtonPressed(ActionEvent event) {
-    	simulationField.clear();
+    	new Thread() {
+    		public void run() {
+    			simulationField.clear();
+    			
     	
-		simulationMinutesUpdate();
-		ToggleButton buttonID = (ToggleButton) event.getSource();
+    			simulationMinutesUpdate();
+    			ToggleButton buttonID = (ToggleButton) event.getSource();
 		
-		String simulateStart = "\n Calculating washing event for "+String.valueOf(timeToSimulate)+" minutes...";
-		simulationField.appendText(simulateStart);
+    			String simulateStart = "\n Calculating washing event for "+String.valueOf(timeToSimulate)+" minutes...";
+    			simulationField.appendText(simulateStart);
+    			
+    			/*if (buttonID.isSelected() == true) {
+    				homeController.diagnosticToggle("Appliance - Dishwasher", 1);
+    				homeController.diagnosticToggle("Dishwasher Water", 1);
+    				homeController.diagnosticToggle("Kitchen Overhead Light", 1);
+    				homeController.diagnosticToggle("Appliance - Water Heater", 1);
+    			} else if (buttonID.isSelected() == false) {
+    				homeController.diagnosticToggle("Appliance - Dishwasher", 2);
+    				homeController.diagnosticToggle("Dishwasher Water", 2);
+    				homeController.diagnosticToggle("Kitchen Overhead Light", 2);
+    				homeController.diagnosticToggle("Appliance - Water Heater", 2);
+    			}*/
+    			
+    			// turning on dishwasher, dishwasher water, and kitchen light
+    			homeController.diagnosticToggle("Appliance - Dishwasher", 1);
+				homeController.diagnosticToggle("Dishwasher Water", 1);
+				homeController.diagnosticToggle("Kitchen Overhead Light", 1);
+				
+				// waiting while dishes are washed
+				pause(10000);
+				
+				// turning off dishwasher, dishwasher water, and kitchen light
+				homeController.diagnosticToggle("Appliance - Dishwasher", 2);
+				homeController.diagnosticToggle("Dishwasher Water", 2);
+				homeController.diagnosticToggle("Kitchen Overhead Light", 2);
+				
+				// turning on water heater 
+				homeController.diagnosticToggle("Appliance - Water Heater", 1);
+				
+				// waiting while water heater heats water
+				pause(10000);
+				
+				// turning water heater off
+				homeController.diagnosticToggle("Appliance - Water Heater", 2);
 		
-		if (buttonID.isSelected() == true) {
-			homeController.diagnosticToggle("Appliance - Dishwasher", 1);
-			homeController.diagnosticToggle("Dishwasher Water", 1);
-			homeController.diagnosticToggle("Kitchen Overhead Light", 1);
-			homeController.diagnosticToggle("Appliance - Water Heater", 1);
-		} else if (buttonID.isSelected() == false) {
-			homeController.diagnosticToggle("Appliance - Dishwasher", 2);
-			homeController.diagnosticToggle("Dishwasher Water", 2);
-			homeController.diagnosticToggle("Kitchen Overhead Light", 2);
-			homeController.diagnosticToggle("Appliance - Water Heater", 2);
-		}
-		
-		// Dishwasher (+ Water Heater) calculation
-		List<Double> totals = simulationCalculation(1800, 6, timeToSimulate/60, 1.0);
-		double tempw = totals.get(0); 
-		double tempg = totals.get(1);
-		double tempc = totals.get(2);
+    			// Dishwasher (+ Water Heater) calculation
+    			List<Double> totals = simulationCalculation(1800, 6, timeToSimulate/60, 1.0);
+    			double tempw = totals.get(0); 
+    			double tempg = totals.get(1);
+    			double tempc = totals.get(2);
 		
 		
-		// Kitchen-light calculation 
-		totals = simulationCalculation(60, 0, timeToSimulate/60, 0.0);
-		totals.set(0, totals.get(0)+tempw);
-		totals.set(1, totals.get(1)+tempg);
-		totals.set(2, totals.get(2)+tempc);
-		totals = roundingData(totals);
+    			// Kitchen-light calculation 
+    			totals = simulationCalculation(60, 0, timeToSimulate/60, 0.0);
+    			totals.set(0, totals.get(0)+tempw);
+    			totals.set(1, totals.get(1)+tempg);
+    			totals.set(2, totals.get(2)+tempc);
+    			totals = roundingData(totals);
 		
-		// TextArea output
-		String watts = "\n Calculated kilowatts used: ";
-		String gallons = "\n Calculated gallons used: ";
-		String cost = "\n Calculated overall cost: ";
-		String addon = Double.toString(totals.get(0));
-		simulationField.appendText(watts+addon);
-		addon = Double.toString(totals.get(1));
-		simulationField.appendText(gallons+addon);
-		addon = Double.toString(totals.get(2));
-		simulationField.appendText(cost+addon);
+    			// TextArea output
+    			String watts = "\n Calculated kilowatts used: ";
+    			String gallons = "\n Calculated gallons used: ";
+    			String cost = "\n Calculated overall cost: ";
+    			String addon = Double.toString(totals.get(0));
+    			simulationField.appendText(watts+addon);
+    			addon = Double.toString(totals.get(1));
+    			simulationField.appendText(gallons+addon);
+    			addon = Double.toString(totals.get(2));
+    			simulationField.appendText(cost+addon);
+    	}
+    	}.start();
     }
 	
     // Calculates for any given simulation
@@ -222,6 +290,7 @@ public class SmartHomeDiagnosticsController implements Initializable{
     
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+    	simulationField.setEditable(false);
 	}
 	
 	public void toggleSimulation2 (ActionEvent event) {
